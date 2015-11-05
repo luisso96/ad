7d467a4c0dc625a5dc2ee54b973e8bc377e2bc43
luisso96 <luisalmendrosrios@gmail.com>
2015-11-05 10:43:38 +0100

    Arreglos y empezar PCategoria

---
+++ PSerpisAd/PSerpisAd/ComboBoxHelper.cs
@@ -0,0 +1,36 @@
+using Gtk;
+using System;
+using System.Collections;
+
+namespace SerpisAd
+{
+	public class ComboBoxHelper
+	{
+		public static void Fill (ComboBox comboBox, QueryResult queryResult)
+		{
+			CellRendererText cellRendererText = new CellRendererText ();
+			comboBox.PackStart (cellRendererText,false);
+			comboBox.SetCellDataFunc (cellRendererText, delegate (CellLayout cell_layout, CellRenderer Cell, TreeModel Tree_Model, TreeIter iter){
+				IList row = (IList)Tree_Model.GetValue(iter, 0);
+				cellRendererText.Text = row[1].ToString();
+			});
+			ListStore listStore = new ListStore (typeof(IList));
+			//TODO localizacion de "sin asignar"
+			IList first = new object [] { null, "<sin asignar>" };
+			TreeIter treeIterFirst = listStore.AppendValues (first);
+			foreach (IList row in queryResult.Rows)
+				listStore.AppendValues(row);
+			comboBox.Model = listStore;
+			//comboBox.Active = 0;
+			comboBox.SetActiveIter (treeIterFirst);
+		}
+
+		public static object GetId (ComboBox comboBox){
+			TreeIter treeIter;
+			comboBox.GetActiveIter (out treeIter);
+			IList row = (IList)comboBox.Model.GetValue (treeIter, 0);
+			return row [0];
+		}
+	}
+}
+
