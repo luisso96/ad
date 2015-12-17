fa44334c489cfb50834e0c57e4d819545eb13787
luisso96 <luisalmendrosrios@gmail.com>
2015-12-17 10:05:27 +0100

    PruebaArticulo muestra valores de la tabla, independientemente de numero de columnas o filas

---
+++ GMySQL/src/main/java/org/institutoserpies/ad/PruebaArticulo.java
@@ -0,0 +1,33 @@
+package org.institutoserpies.ad;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+public class PruebaArticulo {
+	public static void main(String[] args) throws SQLException {
+		Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/dbprueba","root","sistemas");
+		
+		String query = "SELECT * FROM articulo";
+		ResultSet resultset = connection.createStatement().executeQuery(query);
+		ResultSetMetaData resultsetmd = resultset.getMetaData();
+		
+			for (int i = 1; i < resultsetmd.getColumnCount()+1; i++) {
+				System.out.print(resultsetmd.getColumnName(i)+"\t"+"\t");
+			}
+		
+	       while (resultset.next()) {
+	    	   System.out.println("");
+	    	   for (int i = 1; i < resultsetmd.getColumnCount()+1; i++) {
+	    		   System.out.print(resultset.getString(i)+"\t"+"\t");
+			}
+	       }
+		
+		
+		
+		connection.close();
+	}
+}
